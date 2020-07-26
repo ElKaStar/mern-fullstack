848b15c7b06841c99ce29ae7760c525d07f9889c
Elena Tikhonova <licose2019@gmail.com>
2020-07-26 18:29:46 +0300

    build

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
@@ -10,6 +10,13 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.route'))
 app.use('/api/notes', require('./routes/notes.route'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+app.use('/', express.static(path.join(__dirname,'client', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    } )
+}
 
 const PORT = config.get('port') || 5000
 
